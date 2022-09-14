66e818a05851b72ba91b0ffcc37798f65173cb76
PHILANI-MXA <3911755@myuwc.ac.za>
2022-09-14 13:48:02 +0200

    1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="home" class="container-fluid min-vh-100 my-5 p-4 w-100">
+  <div id="home" class="container-fluid min-vh-100  row d-flex justify-content-center my-5 p-5 mr-20">
+
   </div>
 </template>
 
@@ -13,14 +14,21 @@ export default {
 
 <style scoped>
 .container-fluid{
-  background-attachment: fixed;
-  background-position: fixed;
-  background: url(https://i.postimg.cc/7hnVt6gX/tai-s-captures-r5k-TKshp22-M-unsplash.jpg) center
-   center/cover !important;
-  min-width: 100%;
+    background-attachment: fixed;
+    background-position: fixed;
+    background: url() center
+    center/cover !important;
+    min-width: 100%;
 }
 
-
+.img-circle{
+    align-items: center !important;
+    width: 80%;
+    height: 80%;
+    border-radius: 50%;
+    object-fit: cover;
+    margin-right: 500px;
+}
 
 
 
--- src/views/contactView.vue
+++ src/views/contactView.vue
@@ -1,5 +1,5 @@
 <template>
-<div id="about" class="container text-start h-100 justify-content-between overflow-auto">
+<div id="about" class="container text-start min-vh-100 justify-content-between overflow-auto">
 <h2 class="text-center bg-black my-2 p-3 fw-bolder">Contact</h2>
 <div class="row p-4">
 <div class="col-md-6">
@@ -36,52 +36,52 @@
 <div class="col-md-6">
                        <h5 class="text-center text-decoration-none my-2 bg-black text-white">Contact me</h5>
  <form class="w-100 text-black float-start align-items-start" action="https://formspree.io/f/xyyobvab" method="post">
-                                                    <div class="row my-2">
-                                                        <label for="name" class="col-md-1 col-form-label-lg fs-5 text-black"> Name</label>
-                                                        <div class="col-2"></div>
-                                                        <div class="col-8">
-                                                            <input id="name" name="name" type="text" placeholder="Enter your name" class="form-control">
-                                                        </div>
-                                                    </div>
-                                                    <div class="row my-2">
-                                                        <label for="Your location" class="col-md-1 col-form-label-lg fs-5 text-black"> Location</label>
-                                                        <div class="col-2"></div>
-                                                        <div class="col-8">
-                                                            <input id="location" name="location" type="text" placeholder="Enter your location" class="form-control">
-                                                        </div>
-                                                    </div>
-                                                    <div class="row my-2">
-                                                        <label for="time" class="col-md-1 col-form-label-lg fs-5 text-black"> Time</label>
-                                                        <div class="col-2"></div>
-                                                        <div class="col-8">
-                                                            <input id="time" name="time" type="time" placeholder="When do you need me?" class="form-control">
-                                                        </div>
-                                                    </div>
-                                                    <div class="row my-1">
-                                                        <label for="gender" class="col-md-1 col-form-label-lg fs-5 text-black">Company </label>
-                                                        <div class="col-2"></div>
-                                                        <div class="col-8 col-md-6">
-                                                        
-                                                            <span class="ms-3 text-black">Private Company <input type="radio" id="private" name="company" required></span> <div class="col"><span>
-                                                            <span class="ms-3 text-black"> Public Company <input type="radio" id="public" name="company" required></span></span></div>
+                                <div class="row my-2">
+                                    <label for="name" class="col-md-1 col-form-label-lg fs-5 text-black"> Name</label>
+                                    <div class="col-2"></div>
+                                    <div class="col-8">
+                                        <input id="name" name="name" type="text" placeholder="Enter your name" class="form-control">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <label for="message" class="col-md-1 col-form-label-lg fs-5 text-black">Message</label>
-                                <div class="col-2"></div>
-                            <div class="col-8">
-                                    <input id="message" name="message" type="text" placeholder="type your message here" class="form-control">
-                            </div>
-                            </div>
-                            <div class="row my-2">
-                                <label class="col-md-2 col-form-label-lg"></label>
-                                <div class="col-12">
-                                    <label for="submit"></label>
-                                    <input type="submit" value="submit" class="btn text-white bg-black w-100">
-                            </div>
-                            </div>
-                        </form>
-                    </div>
+                                <div class="row my-2">
+                                    <label for="Your location" class="col-md-1 col-form-label-lg fs-5 text-black"> Location</label>
+                                    <div class="col-2"></div>
+                                    <div class="col-8">
+                                        <input id="location" name="location" type="text" placeholder="Enter your location" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="row my-2">
+                                    <label for="time" class="col-md-1 col-form-label-lg fs-5 text-black"> Time</label>
+                                    <div class="col-2"></div>
+                                    <div class="col-8">
+                                        <input id="time" name="time" type="time" placeholder="When do you need me?" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="row my-1">
+                                    <label for="gender" class="col-md-1 col-form-label-lg fs-5 text-black">Company </label>
+                                    <div class="col-2"></div>
+                                    <div class="col-8 col-md-6">
+                                    
+                                        <span class="ms-3 text-black">Private Company <input type="radio" id="private" name="company" required></span> <div class="col"><span>
+                                        <span class="ms-3 text-black"> Public Company <input type="radio" id="public" name="company" required></span></span></div>
+            </div>
+        </div>
+        <div class="row">
+            <label for="message" class="col-md-1 col-form-label-lg fs-5 text-black">Message</label>
+            <div class="col-2"></div>
+        <div class="col-8">
+                <input id="message" name="message" type="text" placeholder="type your message here" class="form-control">
+        </div>
+        </div>
+        <div class="row my-2">
+            <label class="col-md-2 col-form-label-lg"></label>
+            <div class="col-12">
+                <label for="submit"></label>
+                <input type="submit" value="submit" class="btn text-white bg-black w-100">
+        </div>
+        </div>
+    </form>
+</div>
 <hr>
 </div>
 
