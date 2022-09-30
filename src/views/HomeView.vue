f6b67552bf892c525b6ee3dc68313f9bb98705e8
Philani Mxathule <3911755@myuwc.ac.za>
2022-09-30 16:29:05 +0200

    changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="home" class="container h-100 p-5 my-5 d-flex m-auto justify-content-center">
+  <div id="home" class="container h-100 p-5 my-5 d-flex m-auto d-flex justify-content-center align-items-center">
     <div class="row m-auto">
       <div class="col-md-2"></div>
-    <div class="col-md-8">
+    <div class="col-md-8 d-flex justify-content-center align-items-center">
       <img class="img-fluid m-auto" src="https://i.postimg.cc/yxrqP3cZ/Whats-App-Image-2022-09-30-at-10-07-12-AM.jpg" alt=""></div>
     <div class="col-md-2"></div>
   </div>
@@ -25,6 +25,7 @@ img{
  height: 60%;
  aspect-ratio: 0 0;
  margin: auto;
+ background-position: center;
 }
   
   </style>
